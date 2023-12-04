899cbb680db8f650ef476fada94abe857ac7919b
Danil Sidoruk <danilsidoruknv@yandex.ru>
2023-12-04 14:20:00 +0300

    Add simple implementation of interface without exceptions handling

--- hashing_techniques/forms/mainwindow.cpp
+++ hashing_techniques/forms/mainwindow.cpp
@@ -1,5 +1,21 @@
 #include "mainwindow.hpp"
 
+#include "hashing.hpp"
+
 MainWindowForm::MainWindowForm(QWidget *parent) : QMainWindow(parent) {
     ui.setupUi(this);
+
+    QObject::connect(ui.hashButton, &QPushButton::clicked, this, [this]() {
+        auto hash = hashSum(ui.textEdit->text(), {.max = ui.cEdit->text().toLongLong() - 1 });
+        ui.hashLabel->setText(QString::number(hash));
+    });
+    QObject::connect(ui.gammaButton, &QPushButton::clicked, this, [this]() {
+        auto hash = gammaSum(ui.textEdit->text(), {
+                .a = ui.aEdit->text().toLongLong(),
+                .b = ui.bEdit->text().toLongLong(),
+                .c = ui.cEdit->text().toLongLong(),
+                .t = ui.tEdit->text().toLongLong()
+        });
+        ui.gammaLabel->setText(QString::number(hash));
+    });
 }
